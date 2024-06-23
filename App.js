8e3808539fecd0edac00a29a4670fece30a93fa7
santhosh <san.kmr004@gmail.com>
2024-06-23 15:31:41 +0530

    episode-01

---
+++ App.js
@@ -0,0 +1,16 @@
+// const h1 = React.createElement("h1", { id: "heading" }, "Hello from react!");
+// console.log(h1);
+
+const parent = React.createElement("div", { id: "parent" }, [
+  React.createElement("div", { id: "child1" }, [
+    React.createElement("h1", { id: "heading" }, "Iam child1 React Heading H1"),
+    React.createElement("h2", { id: "heading2" }, "Iam child1 H2"),
+  ]),
+  React.createElement("div", { id: "child2" }, [
+    React.createElement("h1", { id: "heading" }, "Iam child2 React Heading H1"),
+    React.createElement("h2", { id: "heading2" }, "Iam child2 H2"),
+  ]),
+]);
+const rootElement = ReactDOM.createRoot(document.getElementById("root"));
+console.log(rootElement);
+rootElement.render(parent);
