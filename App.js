03711e6b4b8ed1f6f80d40366bac91c31d391554
santhosh <san.kmr004@gmail.com>
2024-06-24 22:29:22 +0530

    episode-3

--- App.js
+++ App.js
@@ -1,19 +1,27 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-// const h1 = React.createElement("h1", { id: "heading" }, "Hello from react!");
-// console.log(h1);
+//React Element
+const heading = React.createElement("h1", { id: "heading" }, "React heading💥");
 
-const parent = React.createElement("div", { id: "parent" }, [
-  React.createElement("div", { id: "child1" }, [
-    React.createElement("h1", { id: "heading" }, "Iam child1 React Heading H1"),
-    React.createElement("h2", { id: "heading2" }, "Iam child1 H2"),
-  ]),
-  React.createElement("div", { id: "child2" }, [
-    React.createElement("h1", { id: "heading" }, "Iam child2 React Heading H1"),
-    React.createElement("h2", { id: "heading2" }, "Iam child2 H2"),
-  ]),
-]);
-const rootElement = ReactDOM.createRoot(document.getElementById("root"));
-console.log(rootElement);
-rootElement.render(parent);
+console.log(heading);
+
+//JSX
+const headingUsingJSX = <h1 id="heading">React heading using JSX💥</h1>;
+
+console.log(headingUsingJSX);
+
+//Functional Component
+const TitleComponent = () => <h1>React Title</h1>;
+
+const HeadingComponent = () => (
+  <div id="container">
+    <TitleComponent />
+    <h1 id="heading">React heading using Functional Component💥</h1>
+  </div>
+);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+// root.render(headingUsingJSX);
+root.render(<HeadingComponent />);
